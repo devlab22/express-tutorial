2552d7969820011e0c7645f172c2ba67752f9a48
VENGELHARD <v.engelhard@lighthouse-mcs.de>
2023-10-04 15:50:04 +0200

    sap

--- apiSap.js
+++ apiSap.js
@@ -3,6 +3,36 @@ const axios = require("axios");
 const express = require("express");
 const router = express.Router();
 
+async function getCustomizing(token = null) {
+
+    const auth = token
+
+    const authorization = 'uname:password'
+    if (!auth) {
+        auth = Buffer.from(authorization).toString('base64')
+    }
+
+    const { data } = await axios('http://lhd.lighthouse-it.de:8000/sap/bc/webrfc',
+
+        {
+            params:{
+                'view': 'PERSON',
+                'begda': '20200101',
+                'endda': '20240101',
+                '_FUNCTION': 'Z_GET_CUSTOMIZING',
+                'sap-client': '005'
+            },
+            headers: {
+                'Authorization': `Basic ${auth}`,
+                'Accept': 'application/json'
+            }
+        }
+    )
+
+    return data
+
+}
+
 async function getCountries(name = null) {
 
     if (name === null) {
@@ -16,20 +46,21 @@ async function getCountries(name = null) {
 
 }
 
-async function getSapUsers(token=null) {
+async function getSapUsers(token = null) {
 
     const auth = token
 
     const authorization = 'uname:password'
-    if (!auth){
-        auth  = Buffer.from(authorization).toString('base64')
-    } 
-    
-    const { data } = await axios('http://lhd.lighthouse-it.de:8000/sap/opu/odata/sap/zve_userui5_srv/UserDataSet',
+    if (!auth) {
+        auth = Buffer.from(authorization).toString('base64')
+    }
+
+    const { data } = await axios('http://lhd.lighthouse-it.de:8000/sap/opu/odata/sap/zve_userui5_srv/UserDataSet/?sap-client=005$skip=0&sap-ui-language=DE&sap-ui-xx-devmode=true',
 
         {
             headers: {
-                'Authorization': `Basic ${auth}`
+                'Authorization': `Basic ${auth}`,
+                'Accept': 'application/json'
             }
         }
     )
@@ -52,7 +83,7 @@ router.get('/countries', (req, res, next) => {
     const countryName = req.query.name || null
     const result = {
         count: 0,
-        code: 200,
+        status: 200,
         server: 'express',
         router: 'sap',
         method: 'get country'
@@ -72,7 +103,8 @@ router.get('/countries', (req, res, next) => {
     }
     catch (err) {
         result.msg = err.message
-        result.code = 401
+        result.status = err.response.status
+        result.statusText = err.response.statusText
         res.json(result);
     }
 
@@ -83,35 +115,74 @@ router.get('/sapusers', (req, res, next) => {
 
     const token = req.query.token || null
     const result = {
-        count: 0,
-        code: 200,
+        status: 200,
+        statusText: 'OK',
         server: 'express',
         router: 'sap',
         method: 'get sap users'
     }
 
     try {
-
         (async () => {
 
             try {
                 const data = await getSapUsers(token)
                 result.result = data;
-                result.count = data.length;
                 res.json(result);
             }
             catch (err) {
                 result.msg = err.message
-                result.code = 401
+                result.status = err.response.status
+                result.statusText = err.response.statusText
                 res.json(result);
             }
 
         })()
-
     }
     catch (err) {
         result.msg = err.message
-        result.code = 401
+        result.status = err.response.status
+        result.statusText = err.response.statusText
+        res.json(result);
+    }
+
+})
+
+router.get('/customizing', (req, res, next) => {
+
+    
+    const token = req.query.token || null
+    const result = {
+        status: 200,
+        statusText: 'OK',
+        server: 'express',
+        router: 'sap',
+        method: 'get sap customizing'
+    }
+
+    try{
+
+         (async () => {
+
+            try {
+                const data = await getCustomizing(token)
+                result.result = data;
+                res.json(result);
+            }
+            catch (err) {
+                
+                result.msg = err.message
+                result.statusText = err.response.statusText
+                result.status = err.response.status
+                res.json(result);
+            }
+
+        })()
+    }
+    catch(err){
+        result.msg = err.message
+        result.status = err.response.status
+        result.statusText = err.response.statusText
         res.json(result);
     }
 
